b5630dee10655e6231ee5d5258b220e46955701c
Jorrit <jorrit_l@hotmail.com>
2016-07-19 16:35:51 +0200

    added functionality for differing grid dimensions in the same pixel surface area

--- js/jquery.js
+++ js/jquery.js
@@ -10,10 +10,19 @@ $(document).ready(function(){
 	
 });
 
+
+
 //Draws the grid with a given width and height
 function drawGrid(width, height){
 	var count = 0;
 	var surface = width * height;
+
+	var gridWidth = 320 / width;
+	var gridHeight = 320 / height;
+
+	$(".grid").css("width", gridWidth);
+	$(".grid").css("height", gridHeight);
+
 	while(count < surface) {
 
 		$(".wrapper").append("<div class ='grid'></div>");
