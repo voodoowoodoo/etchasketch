37c605ef6187c13ec6fed3474d2fa80f1b435292
Jorrit <jorrit_l@hotmail.com>
2016-07-19 17:09:07 +0200

    added newgrid functionality

--- js/jquery.js
+++ js/jquery.js
@@ -1,31 +1,52 @@
 $(document).ready(function(){
 	
-	drawGrid(16, 16);
-
-	//Change grid square color permanently when hovering over it.
-	$(".grid").mouseenter(function(){
-		$(this).css("background-color", "black");
-
-	});	
+	defineGrid(16, 16);
 	
 });
 
+//defines the paramaters of the grid divs, sothat they fit in the wrapper
+//the reason that count a surface are in here instead of drawgrid is beacuse of previous version not including a grid refresh button.
+function defineGrid(width, height){
 
-
-//Draws the grid with a given width and height
-function drawGrid(width, height){
 	var count = 0;
 	var surface = width * height;
 
 	var gridWidth = 320 / width;
 	var gridHeight = 320 / height;
 
-	$(".grid").css("width", gridWidth);
-	$(".grid").css("height", gridHeight);
+	drawGrid(count, surface, gridWidth, gridHeight)
 
-	while(count < surface) {
+
+}
+
+//Prompts the user for entering the dimensions for a new grid, and creates that grid
+function gridPrompt(){
+	var dimension = parseInt(prompt("Enter the width / height for your new grid"));
+	
+	$(".wrapper").empty(); //clears the existing grid, preparing for the new draw
+
+	defineGrid(dimension, dimension);
+
+}
+
+
+//Actually draws the grid.
+function drawGrid(c, s, w, h){
+	
+	$(document).ready(function(){
+
+
+		$(".grid").width(w);
+		$(".grid").height(h);
+
+		$(".grid").mouseenter(function(){
+			$(this).css("background-color", "black");
+		});
+	});
+
+	while(c < s) {
 
 		$(".wrapper").append("<div class ='grid'></div>");
-		count += 1;
+		c += 1;
 	}
 }
