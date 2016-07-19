06914ede1cb59b8be740c2db608d920937dd3ee0
Jorrit <jorrit_l@hotmail.com>
2016-07-19 14:20:09 +0200

    Corrected jquery import

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	drawGrid();
 
-	
 });
+
+//Draws the original 16 x 16 grid
+function drawGrid(){
+	var count = 0;
+	while(count < 17) {
+		$(".wrapper").add("<div class ='grid'></div>");
+	}
+}
